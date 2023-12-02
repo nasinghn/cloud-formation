44e0fbba11f087fa93cd86994122d5ff21dad3a3
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-12-02 10:34:22 +0530

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -13,8 +13,8 @@ app.use(cors());
 // MySQL database configuration
 const db = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'shashank',
-    password: 'asd123',
+    user: 'admin',
+    password: 'sanjay12',
     database: 'narender',
 });
 
