c5ce76839d755bbc80cf917d1b1faeeab0a67701
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-12-03 20:44:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 npm install
 npm install -g pm2
 ps -ef | grep /usr/bin/node | awk '{print  $2}' | xargs -n 1 sudo kill -9
-sudo nohup /usr/local/bin/node index.js &
+sudo nohup /usr/local/bin/node /home/ec2-user/cloud-fromation/nodejs/index.js &
 
