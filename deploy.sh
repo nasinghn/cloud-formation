6e8f2a37d13df7f930765112023e5db996993940
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-12-03 22:10:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-sudo cd /home/ec2-user/cloud-formation/nodejs/
-sudo npm install
+sudo npm install --prefix ./nodejs/
 cat /home/ec2-user/db_endpoint | xargs -n 1 -I {}  sudo sed -i  "s/.*host.*/    host: '{}',/" /home/ec2-user/cloud-formation/nodejs/index.js
-sudo nohup /usr/bin/node index.js &
-
+sudo nohup /usr/bin/node ./nodejs/index.js &
