5b599ed83cb24b0eb36c2a355f2a987d79b4ef4c
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-12-03 16:08:15 +0530

    Add files via upload

---
+++ tier3.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS Narender;
+USE Narender;
+CREATE TABLE IF NOT EXISTS Customers (
+    CustomerName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255),
+    Address VARCHAR(255)
+);
